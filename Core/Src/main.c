29badc923af9312efbc0b086cf49c6be495c617a
Alexander Badunov <budeniy74@gmail.com>
2024-12-19 22:30:06 +0300

    задание 3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -46,6 +46,8 @@
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -59,9 +61,6 @@
   * @brief  The application entry point.
   * @retval int
   */
-#include "main.h"
-#include "stm32f4xx.h"
-
 void delay(uint32_t count) {
     while (count--) {
         __NOP();
@@ -69,50 +68,149 @@ void delay(uint32_t count) {
 }
 
 int main(void) {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    GPIOA->MODER &= ~(GPIO_MODER_MODER5_Msk);
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;
+    GPIOA->MODER &= ~GPIO_MODER_MODER5_Msk;
     GPIOA->MODER |= (1U << GPIO_MODER_MODER5_Pos);
-
-    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5);
+    GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
     GPIOA->OSPEEDR |= (0U << GPIO_OSPEEDR_OSPEED5_Pos);
-    GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD5_Msk);
+    GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD5_Msk;
+    GPIOA->MODER &= ~(GPIO_MODER_MODER1_Msk | GPIO_MODER_MODER4_Msk);
+    GPIOA->MODER |= (1U << GPIO_MODER_MODER1_Pos) | (1U << GPIO_MODER_MODER4_Pos);
+    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT1 | GPIO_OTYPER_OT4);
+    GPIOA->OSPEEDR |= (0U << GPIO_OSPEEDR_OSPEED1_Pos) | (0U << GPIO_OSPEEDR_OSPEED4_Pos);
+    GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD1_Msk | GPIO_PUPDR_PUPD4_Msk);
+    GPIOC->MODER &= ~(GPIO_MODER_MODER13_Msk);
+    GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD13_Msk);
+    GPIOC->PUPDR |= GPIO_PUPDR_PUPD13_0;
     while (1) {
-        GPIOA->ODR ^= GPIO_ODR_OD5;
-        delay(1000000);
+        if ((GPIOC->IDR & GPIO_IDR_ID13) == 0) {
+            GPIOA->ODR |= GPIO_ODR_OD5;
+            delay(500000);
+            GPIOA->ODR &= ~GPIO_ODR_OD5;
+            GPIOA->ODR |= GPIO_ODR_OD1;
+            delay(500000);
+            GPIOA->ODR &= ~GPIO_ODR_OD1;
+            GPIOA->ODR |= GPIO_ODR_OD4;
+            delay(500000);
+            GPIOA->ODR &= ~GPIO_ODR_OD4;
+        } else {
+            GPIOA->ODR &= ~(GPIO_ODR_OD1 | GPIO_ODR_OD4 | GPIO_ODR_OD5);
+        }
+        delay(100000);
     }
 }
-
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  /* USER CODE END 3 */
-
 /**
   * @brief System Clock Configuration
   * @retval None
   */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA1 PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
